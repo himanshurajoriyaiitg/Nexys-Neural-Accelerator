867ffc75e16722125e20d2d0fb7110784db3e965
himanshurajoriyaiitg <himanshuiitg2006@gmail.com>
2026-04-24 01:04:08 +0530

    changing the reset logic

---
+++ rtl/reset_sync.sv
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+
+module reset_sync (
+    input  wire clk,
+    input  wire arst_n,
+    output wire rst_n
+);
+
+    (* ASYNC_REG = "TRUE", SHREG_EXTRACT = "NO" *) reg [1:0] sync_ff;
+
+    always @(posedge clk or negedge arst_n) begin
+        if (!arst_n) begin
+            sync_ff <= 2'b00;
+        end else begin
+            sync_ff <= {sync_ff[0], 1'b1};
+        end
+    end
+
+    assign rst_n = sync_ff[1];
+
+endmodule
