31fccd7ec5a1515a588a4c3b862e8877d75c749d
deepak_swami <theprocoder0ds@gmail.com>
2026-05-08 10:42:16 +0530

    Fix comment typo in headless_test function

--- tools/test_gui_headless.py
+++ tools/test_gui_headless.py
@@ -8,7 +8,7 @@ def headless_test():
     args = argparse.Namespace(backend="software", port=None, baud=921600, pad=2, send_interval=0.1, retrain_model=False, fpga_clock_hz=25000000, no_board_display=True, model="cnn")
     app = RealTimeDigitDemo(args)
     
-    # Paint something
+    # Paint something .
     app._update_cell(16, 16, 255)
     app._update_cell(16, 17, 255)
     app.dirty = True
